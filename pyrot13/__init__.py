7a6db63bfd49fff7d857b9895f8dd8ae85ea28a0
Danijel Tasov <m@rbfh.de>
2025-08-08 14:48:50 +0200

    more py less algorithm
    also a lot faster

--- pyrot13/__init__.py
+++ pyrot13/__init__.py
@@ -1,18 +1,13 @@
 """ rot13 rotate letters by 13 place. """
 
+from string import ascii_uppercase, ascii_lowercase
+
+_rot13 = str.maketrans(
+        ascii_uppercase + ascii_lowercase,
+
+        ascii_uppercase[13:] + ascii_uppercase[:13] +
+        ascii_lowercase[13:] + ascii_lowercase[:13]
+)
 
 def rot13(s: str) -> str:
-	""" rot13 returns a copy of s but each letter is rotated by 13 places. """
-	out = []
-	for c in s:
-		if ord("a") <= ord(c) <= ord("z"):
-			x = ord(c) - ord("a") + 13
-			y = ord("a") + x%26
-			out.append(chr(y))
-		elif ord("A") <= ord(c) <= ord("Z"):
-			x = ord(c) - ord("A") + 13
-			y = ord("A") + x%26
-			out.append(chr(y))
-		else:
-			out.append(c)
-	return "".join(out)
+    return s.translate(_rot13)
